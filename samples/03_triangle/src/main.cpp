538086204097e6d2ff3f0b090ee7ad4ef18996da
ryuukk <wat@wat.wat>
2019-03-13 01:32:32 +0100

    Fix CMake

---
+++ samples/03_triangle/src/main.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <string.h>
+#include <Core.h>
+#include <Configuration.h>
+#include <Engine.h>
+
+class MyGame : public arc::IApp
+{
+    void create() override {
+    }
+
+    void update(float dt) override {
+
+    }
+
+    void render(float dt) override {
+
+    }
+
+    void resize(int width, int height) override {
+
+    }
+
+    void dispose() override {
+    }
+
+};
+
+int main( int argc, char** argv )
+{
+    auto config = arc::Configuration();
+    config.windowTitle = "Sample 03 - Triangle";
+    auto myGame = new MyGame();
+    auto engine = new arc::Engine(myGame, config);
+    engine->run();
+
+    return 0;
+}
