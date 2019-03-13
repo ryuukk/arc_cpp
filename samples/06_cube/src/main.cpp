3ac7acac48b741daf1304c08a0532f607ac2a651
ryuukk <wat@wat.wat>
2019-03-13 23:45:38 +0100

    Rotating cube

--- samples/06_cube/src/main.cpp
+++ samples/06_cube/src/main.cpp
@@ -89,7 +89,6 @@ void main()
             //vertices[i++] = cubeTex[tIdx++];
         }
 
-
         std::vector<short> indices = {
                 0, 2, 1, 0, 3, 2, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 15, 14, 12, 14, 13, 16, 17, 18, 16,
                 18, 19, 20, 23, 22, 20, 22, 21
@@ -110,8 +109,9 @@ void main()
     }
 
     void update(float dt) override {
-        _a += dt;
-        _transform = arc::Mat4::identity();
+
+        _a += 0.016f;
+        _transform.set({0,0,0}, arc::Quat::fromAxis({0.5,1,0.5}, _a));
         _cam->update();
     }
 
